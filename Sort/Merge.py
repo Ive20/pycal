c9ec04a7253ebe5925f9dc190f3de11f083727eb
maxwell <lxhomj@gmail.com>
2014-12-02 21:06:07 +0800

    remove ;

--- Sort/Merge.py
+++ Sort/Merge.py
@@ -6,15 +6,15 @@ Created on 2014.05.17
 '''
 import Sort.Input
 def merge(arr1,arr2):
-    arr=[];
+    arr=[]
     while arr1 and arr2:
         if arr1[0]>arr2[0]:
-            arr.append(arr2.pop(0));
+            arr.append(arr2.pop(0))
         else:
-            arr.append(arr1.pop(0));       
-    return arr+arr1+arr2;
+            arr.append(arr1.pop(0))
+    return arr+arr1+arr2
 
-arr=Sort.Input.Input.getinputarr();
+arr=Sort.Input.Input.getinputarr()
 while len(arr)>1:
-    arr.append(merge(arr.pop(0),arr.pop(0)));
-print(arr);
+    arr.append(merge(arr.pop(0),arr.pop(0)))
+print(arr)
